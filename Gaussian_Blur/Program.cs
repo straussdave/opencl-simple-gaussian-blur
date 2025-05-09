c84bdfb5e725a9f2fdb635da6e962abc63732563
Gabriel <wiedermann.gabriel@gmail.com>
2025-05-09 14:18:21 +0200

    added a new visual studio project

---
+++ Gaussian_Blur/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Gaussian_Blur
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
